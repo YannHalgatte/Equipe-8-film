ee6631682f7469a9d8580ac13781053869987f75
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-08 17:16:42 +0200

    Fix index button width after crack

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,10 +1,11 @@
 let bouton_cliqueici = document.querySelectorAll('.accueil_bouton_cliqueici_container div')
-
+let bouton_cliqueici_width = bouton_cliqueici[0].offsetWidth
 for (let i = 0; i < bouton_cliqueici.length; i++) {
   bouton_cliqueici[0].addEventListener(
     'click',
     function(){
       bouton_cliqueici[i].classList.add("crack")
+      bouton_cliqueici[i].style.width = bouton_cliqueici_width+"px";
     }
   )
 }
