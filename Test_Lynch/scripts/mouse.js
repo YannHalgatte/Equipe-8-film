39d83bfe965f034a30e1e8d48e3d81f7879f5133
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-08 14:25:16 +0200

    Custom cursor

--- Test_Lynch/scripts/mouse.js
+++ Test_Lynch/scripts/mouse.js
@@ -1,8 +1,8 @@
-let cont = document.getElementById('container')
+let cont = document.querySelector('html')
 cont.onmousemove = applyCustomPointer;
 let customPointer = document.getElementById('customPointer');
 
-function applyCustomPointer (event) {
-  customPointer.style.left = event.clientX - (customPointer.offsetWidth / 2) + 'px';
-  customPointer.style.top = event.clientY - (customPointer.offsetHeight / 2) + 'px';
+function applyCustomPointer(event) {
+  customPointer.style.left = event.clientX - (customPointer.offsetWidth/2) + 'px';
+  customPointer.style.top = event.clientY - (customPointer.offsetHeight/2) + 'px';
 }
