a7dabd8b46deb590b811fa9a6f339987e8f9c21a
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-09 12:19:18 +0200

    add div space compensation for the button (clique ici)

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,18 +1,25 @@
 class ButtonCliqueIci {
   constructor() {
+    this.button_cliqueici_container = document.querySelector('.homepage_button_cliqueici_container')
     this.button_cliqueici = document.querySelectorAll('.homepage_button_cliqueici_container div')
     this.button_cliqueici_width = this.button_cliqueici[0].offsetWidth
+    this.button_cliqueici_height = this.button_cliqueici[0].offsetHeight
+    this.button_cliqueici_space = document.createElement("div");
   }
   init(){
-    for (let i = 0; i < this.button_cliqueici.length; i++) {
-      this.button_cliqueici[0].addEventListener(
-        'click',
-        function(){
+    this.button_cliqueici[0].addEventListener(
+      'click',
+      function(){
+        MyButtonCliqueIci.button_cliqueici_space.style.height = (MyButtonCliqueIci.button_cliqueici_height + 'px')
+        let button_cliqueici_child = MyButtonCliqueIci.button_cliqueici_container.appendChild(MyButtonCliqueIci.button_cliqueici_space)
+
+        for (let i = 0; i < MyButtonCliqueIci.button_cliqueici.length; i++) {
           MyButtonCliqueIci.button_cliqueici[i].classList.add("crack")
           MyButtonCliqueIci.button_cliqueici[i].style.width = MyButtonCliqueIci.button_cliqueici_width+"px";
         }
-      )
-    }
+      }
+    )
+
   }
 }
 let MyButtonCliqueIci = new ButtonCliqueIci()
@@ -24,13 +31,13 @@ MyButtonCliqueIci.init()
 
 
 
-function playPause(btn,vid){
-	var vid = document.getElementById(vid);
-	if(vid.paused){
-		vid.play();
-		btn.innerHTML = "Pause";
-	} else {
-		vid.pause();
-		btn.innerHTML = "Play";
-	}
-}
+// function playPause(btn,vid){
+// 	var vid = document.getElementById(vid);
+// 	if(vid.paused){
+// 		vid.play();
+// 		btn.innerHTML = "Pause";
+// 	} else {
+// 		vid.pause();
+// 		btn.innerHTML = "Play";
+// 	}
+// }
