8db69bcd856d4497cf5a51e5c1bcc3ad137935d6
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-09 14:22:26 +0200

    Add message after click on the button (clique ici)

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -5,11 +5,13 @@ class ButtonCliqueIci {
     this.button_cliqueici_width = this.button_cliqueici[0].offsetWidth
     this.button_cliqueici_height = this.button_cliqueici[0].offsetHeight
     this.button_cliqueici_space = document.createElement("div");
+    this.button_cliqueici_message = document.querySelector('.homepage_button_cliqueici_message span')
   }
   init(){
     this.button_cliqueici[0].addEventListener(
       'click',
       function(){
+        MyButtonCliqueIci.button_cliqueici_message.classList.remove("displayNone")
         MyButtonCliqueIci.button_cliqueici_space.style.height = (MyButtonCliqueIci.button_cliqueici_height + 'px')
         let button_cliqueici_child = MyButtonCliqueIci.button_cliqueici_container.appendChild(MyButtonCliqueIci.button_cliqueici_space)
 
