60f58c3872c0899958e4f6a33d1ad6790b30e57a
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-09 09:40:52 +0200

    Fix index button position after crack + header menu

--- scripts/accueil.js
+++ scripts/accueil.js
@@ -1,11 +1,19 @@
-let bouton_cliqueici = document.querySelectorAll('.accueil_bouton_cliqueici_container div')
-let bouton_cliqueici_width = bouton_cliqueici[0].offsetWidth
-for (let i = 0; i < bouton_cliqueici.length; i++) {
-  bouton_cliqueici[0].addEventListener(
-    'click',
-    function(){
-      bouton_cliqueici[i].classList.add("crack")
-      bouton_cliqueici[i].style.width = bouton_cliqueici_width+"px";
+class BoutonCliqueIci {
+  constructor() {
+    this.bouton_cliqueici = document.querySelectorAll('.accueil_bouton_cliqueici_container div')
+    this.bouton_cliqueici_width = this.bouton_cliqueici[0].offsetWidth
+  }
+  init(){
+    for (let i = 0; i < this.bouton_cliqueici.length; i++) {
+      this.bouton_cliqueici[0].addEventListener(
+        'click',
+        function(){
+          MonBoutonCliqueIci.bouton_cliqueici[i].classList.add("crack")
+          MonBoutonCliqueIci.bouton_cliqueici[i].style.width = MonBoutonCliqueIci.bouton_cliqueici_width+"px";
+        }
+      )
     }
-  )
+  }
 }
+let MonBoutonCliqueIci = new BoutonCliqueIci()
+MonBoutonCliqueIci.init()
