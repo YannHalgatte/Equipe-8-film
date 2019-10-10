b92023694a32784a3756de642bb49090b8cda4d2
YannHalgatte <halgatte.yann@gmail.com>
2019-10-10 18:40:07 +0200

    adding sound to the loading page

--- scripts/Chargement.js
+++ scripts/Chargement.js
@@ -1,71 +1,41 @@
-document.getElementsByClassName("button").onclick = function(){buttoned()};
-function buttoned() {
 
+let chargement_button = document.querySelector('button')
+let chargement_button_alreadyClicked = 0
+let chargement_button_autorizeToClick = false
+
+const rire = new Audio ('medias/rireduja.mp3')
+
+function clock(){
+  chargement_button_autorizeToClick=true
+  rire.loop = false;
+        chargement_button.style.cursor = "pointer"
+}
+function menuder(){
+    document.getElementsByClassName('divfront').classList.add('animation');
 }
 
-
-
-
-const rire = new Audio ('../media/rireduja.mp3')
-               document.addEventListener(
-                   'click',
-                   ()=>{
-                       rire.play();
-                       audio.loop = true;
-               }
-               )
-
-
-
-
-let cracked = document.querySelectorAll('.cracked span')
-let red = document.querySelector('.red')
-
-for (let i = 0; i < cracked.length; i++) {
-  cracked[0].addEventListener(
-    'click',
-    function(){
-      if (destroyMode) {
-        console.log('test')
-        cracked[i].classList.add("crack")
-        cracked[i].classList.add("absolute")
-      }
-    }
-  )
-}
-
-
-let backgroundDiv = document.querySelectorAll('.background div')
-let background = document.querySelector('.background')
-for (let i = 0; i < backgroundDiv.length; i++) {
-  background.addEventListener(
-    'click',
-    function(){
-      backgroundDiv[i].classList.add("crack")
-      setTimeout(function(){background.classList.add('displayNone')}, 750)
-    }
-  )
-}
-
-
-
-
-
-
-
-
-let destroyMode = false
-let changeMode = document.querySelector('.changeMode')
-changeMode.addEventListener(
+chargement_button.addEventListener(
   'click',
-  function(){
-    if (destroyMode) {
-      destroyMode=false
-      console.log('destroy false')
+  ()=>{
+    if (chargement_button_alreadyClicked==0) {
+      chargement_button.style.cursor = "wait"
+      chargement_button_alreadyClicked += 1
+      console.log('1e click')
+      rire.play();
+      rire.loop = true;
+      setTimeout(clock, 6500);
+      menuder()
+
+      console.log(chargement_button_autorizeToClick)
     }
-    else {
-      destroyMode=true
-      console.log('destroy true')
+    else if((chargement_button_alreadyClicked==1) && (chargement_button_autorizeToClick==true)){
+      console.log('3e click')
+      rire.loop = false;
+      document.location.href="index.html"
+
+    }
+    else if(chargement_button_alreadyClicked==1){
+      console.log('2e click')
     }
   }
 )
